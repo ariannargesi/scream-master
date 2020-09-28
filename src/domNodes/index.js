ed6ce225a8394d4eaedba8d7eb79da6f690172f0
Arian Nargesi <ariannargesi@gmail.com>
2020-09-28 23:51:52 +0330

    update nodes items

--- src/domNodes/index.js
+++ src/domNodes/index.js
@@ -4,12 +4,20 @@ const nodes = {
     character: document.querySelector('.character'),
     score: document.querySelector('.score'),
     startGame: document.querySelector('.start-game'),
+    gameOver: document.querySelector('.game-over'),
+    startBtn: document.querySelector('.start'),
+    restartBtn: document.querySelector('.restart'),
+    backToMainBtn: document.querySelector('.back'),
+    gameOverScore: document.querySelector('.player-score'),
+    gameOverRate: document.querySelector('.score-rate'),
     soundLevelNum: document.querySelector('.sound-level-number'),
     inputSlider: document.querySelector('.slider'),
     soundLevelHint: document.querySelector('.sound-level-hint'),
     sensitivity: document.querySelector('.sensitivity'),
+    feedback: document.querySelector('.feedback'),
     feedbackSend: document.querySelector('.feedback-send'),
-    feedbackTextarea: document.querySelector('.feedback-textarea')
+    feedbackTextarea: document.querySelector('.feedback-textarea'),
+    socials: document.querySelector('.socials')
 }
 
 export default nodes
