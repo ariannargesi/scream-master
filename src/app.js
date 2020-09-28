da3afe1dad1731cc66ec64d90abc920d92159a36
Arian Nargesi <ariannargesi@gmail.com>
2020-09-28 23:50:44 +0330

    improving gameplay

--- src/app.js
+++ src/app.js
@@ -1,25 +1,71 @@
+
 import getMicSoundLevel from './audio'
 import states from './state'
 import nodes from './domNodes'
 import createHedges from './hedges'
 import './events'
-import { RectCircleColliding, setBallPosition, scoreRating } from './funcs'
+import { RectCircleColliding, setBallPosition, clearContainer, scoreRating, getBrowserName } from './funcs'
 
+let soundLevel = null 
+let intervalId = null 
+const deviceHeight = screen.height
+const deviceWith = screen.width 
+const browserName = getBrowserName()
+// preventing the game from running 
+const preventRunning = () => {
+    states.gameInPlay = null 
+    states.gameOver = null 
+}
 
+// prevent running the app on edge and safari 
+if( browserName === "edge" || browserName === 'safari'){
+        preventRunning()
+        clearContainer()
+        // showign a propper message
+        const h1 = document.createElement('h1')
+        const h2 = document.createElement('h2')
+        h1.innerText = "sorry my friend !!!"
+        h2.innerText = "you need to open this game with chrome or Firefox web browser :)"
+        nodes.container.appendChild(h1)
+        nodes.container.appendChild(h2)
+}
+if(deviceWith < 768){
+    preventRunning()
+    clearContainer()
+
+    nodes.container.style.display = "flex"
+    nodes.container.style.flexDirection = "column"
+    nodes.container.style.justifyContent = "space-between"
+
+    const h4 = document.createElement('h1')
+    const p = document.createElement('p')
+    const span = document.createElement("span")
+    h4.innerText  = "sorry my friend!"
+    p.innerText = "for the best game experience, I decided to only let users with a large screen device, such as tablet or laptop to play this game. please come back with a bigger device"
+    span.innerText = "I will wait for you"    
+    span.style.color = 'rgb(0, 255, 0)'
+    nodes.container.appendChild(h4)
+    nodes.container.appendChild(p)
+    nodes.container.appendChild(span)
+    
+}
+// set the ball speed based on user device height 
+const ballSpead = Math.floor(deviceHeight / 200)
+states.gameSpeed = ballSpead
+// set passages height 
+states.passageHeight = deviceHeight / 3.5
 //always contain the latest value of microphone sound level
-let soundLevel = 0
 getMicSoundLevel((x) => {
     if (x) soundLevel = Math.round(x * states.sensitivity)
 })
 
-let intervalId
-
 document.addEventListener('keydown', (e) => {
     const key = e.keyCode
-    if (key === 32) {
+    if (key === 32 && document.activeElement != nodes.feedbackTextarea ) {
         startGame()
     }
 })
+
 
 nodes.inputSlider.addEventListener('input', (e) => {
     let value = e.target.value / 1000
@@ -35,6 +81,8 @@ const startGame = () => {
         nodes.startGame.style.display = 'none'
         nodes.soundLevelHint.style.display = "none"
         nodes.sensitivity.style.display = "none"
+        nodes.feedback.style.display = "none"
+        nodes.socials.style.display = "none"
         states.gameInPlay = true
         if (states.gameOver === false)
             intervalId = setInterval(() => {
@@ -43,7 +91,11 @@ const startGame = () => {
             }, 2000)
         requestAnimationFrame(updateScreen)
     }
+    // set ball position for center of the page 
+    setBallPosition(deviceHeight / 2 - 25 )
 }
+
+nodes.startBtn.addEventListener('click', startGame)
 
 const updateScreen = () => {
 
@@ -114,7 +166,7 @@ const reStart = () => {
     states.playerScore = 0
     states.gameInPlay = false 
     states.gameOver = false
-    nodes.startGame.style.display = 'none'  
+    nodes.gameOver.style.display = 'none'  
     clearInterval(intervalId)
     // clear screen
     const hedges = document.getElementsByClassName('hedge')
@@ -125,33 +177,24 @@ const reStart = () => {
     startGame()
 }
 
-const gameOver = () => {
+const gameOver = () =>  {
     states.gameOver = true
     states.gameInPlay = false
-    
     nodes.sensitivity.style.display = "flex"
     nodes.soundLevelHint.style.display = 'block'
-
-    nodes.startGame.innerHTML = "<p class='start-game-text'>Game Over</p>"
-    const restart = document.createElement('button')
-    restart.setAttribute('class', 'restart')
-    restart.addEventListener('click', reStart)
-    restart.innerText = 'Try Again'
-    const score = document.createElement('p')
-    score.innerText = 'You Score is: ' + states.playerScore
-
-    const rateEl = document.createElement('p')
-    const rateText = scoreRating(states.playerScore)
-
-    rateEl.innerText = rateText
-
+    nodes.gameOver.style.display = "flex"
+    nodes.startGame.style.display = "none"
+    nodes.restartBtn.addEventListener('click', reStart)
+    nodes.backToMainBtn.addEventListener('click', () => {
+        nodes.gameOver.style.display = "none"
+        nodes.startGame.style.display = "flex"
+        nodes.feedback.style.display = "block"
+        nodes.socials.style.display = "block"
+    })
+    nodes.gameOverScore.innerText = "your score is: " + states.playerScore
+    nodes.gameOverRate. innerText = scoreRating(states.playerScore)
 
     
-
-    nodes.startGame.appendChild(restart)
-    nodes.startGame.appendChild(score)
-    nodes.startGame.appendChild(rateEl)
-    nodes.startGame.style.display = 'flex'
 }
 
 setInterval(() => {
@@ -166,8 +209,8 @@ setInterval(() => {
     nodes.soundLevelNum.setAttribute('class', className)
 }, 30)
 setInterval(() => {
+    if(states.gameInPlay)
     states.playerScore++
 }, 30)
-
 
 
